26861cb4b96348603e1422705446f8549ec46d0f
Nadin Severin <nadinlikes2018@gmail.com>
2022-11-24 17:29:01 +0200

    add last changes

--- src/js/product-modal.js
+++ src/js/product-modal.js
@@ -1,16 +1,16 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    //  openModalBtn1: document.querySelector('[data-modal-open1]'),
-    //  openModalBtn2: document.querySelector('[data-modal-open2]'),
+    openModalBtn: document.querySelector('[data-modal-product]'),
+    openModalBtn1: document.querySelector('[data-modal-product1]'),
+    openModalBtn2: document.querySelector('[data-modal-product2]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  //   refs.openModalBtn1.addEventListener('click', toggleModal);
-  //   refs.openModalBtn2.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn1.addEventListener('click', toggleModal);
+  refs.openModalBtn2.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-product-hidden');
